301deb29bef000f841c35fa698571a0f038f01f1
Sevrus <book-n-troll@nordnet.fr>
2023-07-30 16:20:55 +0200

    Added password hash for user creation

--- src/routes/users/createUser.js
+++ src/routes/users/createUser.js
@@ -1,10 +1,17 @@
 const {User} = require('../../db/sequelize');
 const {ValidationError, UniqueConstraintError} = require('sequelize');
 const auth = require('../../auth/auth');
+const bcrypt = require('bcrypt');
 
 module.exports = (app) => {
     app.post('/api/users', auth, (req, res) => {
-        User.create(req.body)
+        bcrypt.hash(req.body.password, 10)
+            .then(hashedPassword => {
+                return User.create({
+                    email: req.body.email,
+                    password: hashedPassword
+                });
+            })
             .then(user => {
                 const message = `L'utilisateur' ${req.body.id} a bien été crée.`
                 res.json({message, data: user})
