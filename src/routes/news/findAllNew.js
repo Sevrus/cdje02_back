a56de6c28286f6681ccfc80455f0cd78b0d0c2fa
Solwen02 <121789865+Solwen02@users.noreply.github.com>
2023-08-10 17:29:08 +0200

    added findAll with limit

--- src/routes/news/findAllNew.js
+++ src/routes/news/findAllNew.js
@@ -2,14 +2,28 @@ const { News } = require('../../db/sequelize');
 
 module.exports = (app) => {
     app.get('/api/news', (req, res) => {
-        News.findAll()
-            .then(news => {
-                const message = `La liste des articles a bien été récupérée.`;
-                res.json({ message, data: news });
-            })
-            .catch(error => {
-                const message = `La liste des articles n'a pas pu être récupérée. Veuillez réessayer plus tard.`;
-                res.status(500).json({ message, data: error });
-            });
+
+        if (req.query.limit) {
+            const limit = parseInt(req.query.limit);
+            return News.findAll({ limit: limit })
+                .then(news => {
+                    const message = `La liste des ${limit} articles a bien été récupérée.`;
+                    res.json({ message, data: news });
+                })
+                .catch(error => {
+                    const message = 'La liste des articles n\'a pas pu être récupérée. Veuillez réessayer plus tard.';
+                    res.status(500).json({ message, data: error });
+                });
+        } else {
+            News.findAll()
+                .then(news => {
+                    const message = 'La liste des articles a bien été récupérée.';
+                    res.json({ message, data: news });
+                })
+                .catch(error => {
+                    const message = 'La liste des articles n\'a pas pu être récupérée. Veuillez réessayer plus tard.';
+                    res.status(500).json({ message, data: error });
+                });
+        }
     });
 };
